c87a4a539fa59897278634a1a811788a7be5211c
xavierau <xavier.au@gmail.com>
2023-11-22 15:22:25 +0800

    update

--- creative_edu.py
+++
@@ -1,8 +0,0 @@
-from langchain.document_loaders import UnstructuredURLLoader
-
-loader = UnstructuredURLLoader(
-    urls=["https://creative.edu.hk"], mode="elements", strategy="fast",
-)
-docs = loader.load()
-
-print(docs)
